cf46d7d6152067aac2bd7c7d68028a8edd224302
mono <mono@mono0x.net>
2023-02-20 17:00:16 +0000

    deno fmt

--- actions/Move Up Current Line.js
+++ actions/Move Up Current Line.js
@@ -1,17 +1,17 @@
-const [start, end] = editor.getSelectedLineRange()
-const currentLine = editor.getTextInRange(start, end)
+const [start, end] = editor.getSelectedLineRange();
+const currentLine = editor.getTextInRange(start, end);
 
-const text = editor.getText()
+const text = editor.getText();
 
-const prevEnd = start - 1
+const prevEnd = start - 1;
 if (prevEnd > 0) {
-    let prevStart = text.lastIndexOf('\n', prevEnd - 1)
-    if (prevStart === -1) {
-        prevStart = 0
-    } else {
-        prevStart += 1
-    }
-    const prevLine = editor.getTextInRange(prevStart, prevEnd)
-    editor.replaceTextInRange(prevStart, end, `${currentLine}\n${prevLine}`)
-    editor.setSelectedRange(prevStart)
+  let prevStart = text.lastIndexOf("\n", prevEnd - 1);
+  if (prevStart === -1) {
+    prevStart = 0;
+  } else {
+    prevStart += 1;
+  }
+  const prevLine = editor.getTextInRange(prevStart, prevEnd);
+  editor.replaceTextInRange(prevStart, end, `${currentLine}\n${prevLine}`);
+  editor.setSelectedRange(prevStart);
 }
--- actions/Select Current Line.js
+++ actions/Select Current Line.js
@@ -1,9 +1,9 @@
 // https://1writerapp.com/actiondir/action/4f0e3
-const [start, end] = editor.getSelectedLineRange()
+const [start, end] = editor.getSelectedLineRange();
 
-let eol = start + end
-if (editor.getTextInRange(eol - 1, 1) === '\n') {
-    eol = eol - 1
+let eol = start + end;
+if (editor.getTextInRange(eol - 1, 1) === "\n") {
+  eol = eol - 1;
 }
 
-editor.setSelectedRange(start, eol - start)
+editor.setSelectedRange(start, eol - start);
